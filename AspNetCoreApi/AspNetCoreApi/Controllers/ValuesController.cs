74e72bfb817f9ad805bb8a63b8a2556ae804728d
Shi Hao Wang <st890409@hotmail.com>
2017-03-19 20:28:28 +0800

    Web 負載與效能測試
    負載與效能測試專案內容

---
+++ AspNetCoreApi/AspNetCoreApi/Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreApi.Models;
+
+namespace AspNetCoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class LoginController : Controller
+    {
+        [HttpPost]
+        public Account Login([FromBody]Account item)
+        {
+            return item;
+        }
+    }
+
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
